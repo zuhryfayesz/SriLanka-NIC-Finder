a9ec85aa2aa71f27a32e7312d73b1ea3c90dd272
zuhryfayesz <zuhry@pickme.lk>
2016-12-21 12:23:56 +0530

    Initial Commit

---
+++ NIC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NIC
+//
+//  Created by Zuhry Fayesz on 12/21/16.
+//  Copyright © 2016 Zuhry Fayesz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
